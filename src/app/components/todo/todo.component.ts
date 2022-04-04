c47675abc8f7f514ec3d916184dc879b69ed9aa2
Paulo Antonio Mendes <thelukgaming333@gmail.com>
2022-04-04 16:39:17 -0300

    Movendo todos entre arrays

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -10,16 +10,57 @@ import {
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep']
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog']
+  // todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep']
+  // done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog']
+  todos = [
+    {
+      id: 1,
+      todo_name: 'Segunda-Feira',
+      todos_itens: [
+        'Get to work',
+        'Pick up groceries',
+        'Go home',
+        'Fall asleep'
+      ]
+    },
+    {
+      id: 2,
+      todo_name: 'Terca-Feira',
+      todos_itens: [
+        'Meu amigo tijolo',
+        'Pick up groceries',
+        'Go home',
+        'Fall asleep'
+      ]
+    },
+    {
+      id: 3,
+      todo_name: 'Quarta-Feira',
+      todos_itens: [
+        'Get to work',
+        'Pick up groceries',
+        'Go home',
+        'Fall asleep'
+      ]
+    },
+    {
+      id: 4,
+      todo_name: 'Quinta-feira',
+      todos_itens: [
+        'Meu amigo tijolo',
+        'Pick up groceries',
+        'Go home',
+        'Fall asleep'
+      ]
+    }
+  ]
   constructor () {}
 
   ngOnInit (): void {}
-  drop (event: CdkDragDrop<string[]>) {
+  drop (event: any, index: any) {
     if (event.previousContainer === event.container) {
       moveItemInArray(
-        event.container.data,
+        this.todos[index].todos_itens,
         event.previousIndex,
         event.currentIndex
       )
@@ -28,12 +69,13 @@ export class TodoComponent implements OnInit {
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex
+        index
       )
     }
   }
-  newItem($event: any){
-    this.todo.push($event.target.value)
+  newItem ($event: any, index: any) {
+    // console.log(this.todos[index])
+    this.todos[index].todos_itens.push($event.target.value)
     $event.target.value = ''
     // console.log()
   }
